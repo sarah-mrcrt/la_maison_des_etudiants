e6fe9778c2b7bc4ae3c2f31346026e098c2de559
unknown <mauriaucourt.sarah@gmail.com>
2022-12-29 02:42:10 +0100

    page 404

--- wp-content/themes/theme-maison-des-etudiants/404.php
+++ wp-content/themes/theme-maison-des-etudiants/404.php
@@ -11,48 +11,16 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'theme-maison-des-etudiants' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'theme-maison-des-etudiants' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'theme-maison-des-etudiants' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$theme_maison_des_etudiants_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'theme-maison-des-etudiants' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$theme_maison_des_etudiants_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+		<section class="section-hero">
+			<figure>
+					<img class="article-image" src="<?php bloginfo('template_directory'); ?> /images/erreur-404.jpg" alt="Erreur 404">
+				<figcaption>
+					<h1>Erreur 404</h1>
+					<p>Oups ! Cette page ne peut pas être trouvée.</p>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">Accueil</a>
+				</figcaption>
+			</figure>
+		</section>
 
 	</main><!-- #main -->
 
