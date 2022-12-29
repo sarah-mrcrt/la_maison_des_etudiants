1e25e33a8ed639595e5867843ae7d0afa2388e98
unknown <mauriaucourt.sarah@gmail.com>
2022-12-29 19:30:29 +0100

    fix wordpress online pb

--- wp-content/themes/theme-maison-des-etudiants/404.php
+++ wp-content/themes/theme-maison-des-etudiants/404.php
@@ -4,16 +4,17 @@
  *
  * @link https://codex.wordpress.org/Creating_an_Error_404_Page
  *
- * @package La_maison_des_étudiants
+ * @package zefdrg
  */
 
 get_header();
 ?>
 
 	<main id="primary" class="site-main">
+
 		<section class="section-hero">
 			<figure>
-					<img class="article-image" src="<?php bloginfo('template_directory'); ?> /images/erreur-404.jpg" alt="Erreur 404">
+					<img class="article-image" src="<?php echo esc_url( get_template_directory_uri() . '/images/erreur-404.jpg' ); ?>" alt="Erreur 404">
 				<figcaption>
 					<h1>Erreur 404</h1>
 					<p>Oups ! Cette page ne peut pas être trouvée.</p>
--- wp-content/themes/theme-maison-des-etudiants/footer.php
+++ wp-content/themes/theme-maison-des-etudiants/footer.php
@@ -11,60 +11,61 @@
 
 ?>
 
-		<footer>
-			<header class="container">
-				<h2>Nous retrouver.</h2>
-				<div class="flex">
-					<article class="col-5">
-						<h3>Suivez-nous</h3>
-						<div>
-							<ul>
-								<li><a href="#" aria-label="Behance" rel="noopener" target="_blank">Facebook</a></li>
-								<li><a href="#" aria-label="Instagram" rel="noopener nofollow" target="_blank">Instagram</a>
-								<li><a href="#" aria-label="Twitter" rel="noopener" target="_blank">Twitter</a></li>
-							</ul>
-							<ul>
-								<li><a href="#" aria-label="Linkedin" rel="noopener" target="_blank">Linkedin</a>
-								</li>
-								<li><a href="#" aria-label="Vimeo" rel="noopener" target="_blank">Vimeo</a></li>
-							</ul>
-						</div>
-					</article>
+			<footer id="colophon" class="site-footer">
+				<header class="container">
+					<h2>Nous retrouver.</h2>
+					<div class="flex">
+						<article class="col-5">
+							<h3>Suivez-nous</h3>
+							<div>
+								<ul>
+									<li><a href="" aria-label="Behance" rel="noopener" target="_blank">Facebook</a></li>
+									<li><a href="" aria-label="Instagram" rel="noopener nofollow"
+											target="_blank">Instagram</a>
+									<li><a href="" aria-label="Twitter" rel="noopener" target="_blank">Twitter</a></li>
+								</ul>
+								<ul>
+									<li><a href="" aria-label="Linkedin" rel="noopener" target="_blank">Linkedin</a>
+									</li>
+									<li><a href="" aria-label="Vimeo" rel="noopener" target="_blank">Vimeo</a></li>
+								</ul>
+							</div>
+						</article>
 
-					<article class="col-7">
-						<h3>Contactez-nous</h3>
-						<address>
-							Par mail
-							<a href="mailto:hello@maisondesetudiants.fr" aria-label="Mail" rel="noopener"
-								target="_blank">hello@maisondesetudiants.fr</a>
-							, par téléphone
-							<a href="tel:+0600000000" rel="noopener">06.xx.xx.xx.xx</a>
-							, en vrai
-							<a href="#">Avenue Michel Rondet, 59135 Wallers</a>
-						</address>
-					</article>
-				</div>
-			</header>
+						<article class="col-7">
+							<h3>Contactez-nous</h3>
+							<address>
+								Par mail
+								<a href="mailto:hello@maisondesetudiants.fr" aria-label="Mail" rel="noopener"
+									target="_blank">hello@maisondesetudiants.fr</a>
+								, par téléphone
+								<a href="tel:+0600000000" rel="noopener">06.xx.xx.xx.xx</a>
+								, en vrai
+								<a href="#">Avenue Michel Rondet, 59135 Wallers</a>
+							</address>
+						</article>
+					</div>
+				</header>
 
-			<article class="site-information">
-				<?php
-					wp_nav_menu(
-						array(
-						'theme_location' => 'menu-2', // l'identifiant du menu
-						'container_class' => 'menu2', // la classe de la div contenant le menu
-						'container_id' => 'menu2', // l'id de l'élément contenant le menu
-						'menu_class' => 'menu2', // la classe de l'ul
-						'menu_id' => 'menu2', // l'id de l'ul
-						'depth' => 1, // profondeur du menu
-						)
-					);
-				?><!-- #menu-2 footer -->
-				<p>© <?php echo get_bloginfo( 'name' ); ?> | 2022</p>
-			</article>
-		</footer><!-- #colophon -->
+				<div class="site-info">
+					<?php
+						wp_nav_menu(
+							array(
+							'theme_location' => 'menu-2', // l'identifiant du menu
+							'container_class' => 'menu2', // la classe de la div contenant le menu
+							'container_id' => 'menu-2', // l'id de l'élément contenant le menu
+							'menu_class' => 'menu-2', // la classe de l'ul
+							'menu_id' => 'menu-2', // l'id de l'ul
+							'depth' => 1, // profondeur du menu
+							)
+						);
+					?><!-- #menu-2 footer -->
+					<p>© <?php echo get_bloginfo( 'name' ); ?> | 2022</p>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
-<?php wp_footer(); ?>
+		<?php wp_footer(); ?>
 
-</body>
+	</body>
 </html>
